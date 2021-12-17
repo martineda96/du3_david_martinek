38b14d45de990245e4a75d8033e564a77ceb0427
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-12-17 22:39:07 +0100

    Add files via upload
    - Načtena vstupní data (korektně)
    - Ošetření neexistujícího souboru nebo cesty
    Vstup s kontejnery obsahuje ještě koncovku JSON, třeba po půlhodině pátrání opravit

--- du3_david_martinek/du3.py
+++ du3_david_martinek/du3.py
@@ -1,10 +1,12 @@
 from funkce import *
 from statistics import mean, median
 
+
 MAX_VZDALENOST = 10000
 
 #nahrani souboru
-
+adresa = otevreni_souboru("adresy.geojson")
+kontejnery = otevreni_souboru("kontejnery.geojson.json")
 #prevod adres do S-JTSK
 
 #cyklus - pro kazdou adresu najit nejblizsi kontejner
