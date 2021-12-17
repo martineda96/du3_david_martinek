afc9d9f76cb1391d2e67b235dd0192ad25050901
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-12-17 21:53:08 +0100

    Add files via upload
    - Vytvořen návrh hrubé kostry celého programu
    - Definování funkcí, které určitě budou použity
    - Importování knihoven, které určitě budou použity
    Commitu předcházelo studium transformačních funkcí. Pro změnu projekce nakonec byl zvolen Transformer knihovny pyproj.

--- du3_david_martinek/du3.py
+++ du3_david_martinek/du3.py
@@ -1,3 +1,18 @@
-import json
+from funkce import *
+from statistics import mean, median
 
 MAX_VZDALENOST = 10000
+
+#nahrani souboru
+
+#prevod adres do S-JTSK
+
+#cyklus - pro kazdou adresu najit nejblizsi kontejner
+
+#prirazeni kontejneru k adresam (tvorba vystupniho souboru)
+
+#domovni kontejnery? (bonus)
+
+# prumer + median? (bonus)
+
+#vystup (printy)
--- du3_david_martinek/funkce.py
+++ du3_david_martinek/funkce.py
@@ -1,2 +1,16 @@
 from math import sqrt
 import json
+from pyproj import Transformer
+
+def vypocet_vzdalenosti(x_adresa,x_kontejner,y_adresa,y_kontejner):
+    return sqrt((x_adresa-x_kontejner)*(x_adresa-x_kontejner)+(y_adresa - y_kontejner)*(y_adresa - y_kontejner))
+
+def otevreni_souboru(soubor):
+    with open(soubor, encoding="utf-8") as vstup:
+        return json.load(vstup)
+
+def zmena_projekce(x_souradnice,y_souradnice):
+    transformace = Transformer.from_crs(4326,5514, always_xy=True)
+    poloha_xy = transformace.transform(x_souradnice,y_souradnice)
+    return poloha_xy
+    
