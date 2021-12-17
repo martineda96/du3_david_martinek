b2e2c362e29cfcb7f50be0855141d9e38893e106
martineda96 <93780022+martineda96@users.noreply.github.com>
2021-12-17 23:13:23 +0100

    Add files via upload
    - Separace volných kontejnerů od domovních

--- du3_david_martinek/du3.py
+++ du3_david_martinek/du3.py
@@ -1,12 +1,18 @@
 from funkce import *
 from statistics import mean, median
 
-
 MAX_VZDALENOST = 10000
 
 #nahrani souboru
 adresa = otevreni_souboru("adresy.geojson")
-kontejnery = otevreni_souboru("kontejnery.geojson.json")
+kontejnery = otevreni_souboru("kontejnery.geojson")
+
+kontejnery_volne = []
+pocet_kontejneru = 0
+for koš in kontejnery["features"]:
+    if koš ["properties"]["PRISTUP"] != "obyvatelům domu":
+        kontejnery_volne.append(koš)
+        pocet_kontejneru += 1
 #prevod adres do S-JTSK
 
 #cyklus - pro kazdou adresu najit nejblizsi kontejner
@@ -18,3 +24,5 @@ kontejnery = otevreni_souboru("kontejnery.geojson.json")
 # prumer + median? (bonus)
 
 #vystup (printy)
+
+print(f"Načteno {pocet_kontejneru} volných kontejnerů na tříděný odpad.")
